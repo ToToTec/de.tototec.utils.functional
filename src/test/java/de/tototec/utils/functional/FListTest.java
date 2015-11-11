731aa304473e8036fc5b539809ee1c483175c694
Tobias Roeser <le.petit.fou@web.de>
2015-11-11 11:17:33 +0100

    Converted tests to use LambdaTest

--- src/test/java/de/tototec/utils/functional/FListTest.java
+++ src/test/java/de/tototec/utils/functional/FListTest.java
@@ -5,97 +5,79 @@ import static de.tototec.utils.functional.FList.mkString;
 import static org.testng.Assert.assertEquals;
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import de.tobiasroeser.lambdatest.testng.FreeSpec;
+import de.tobiasroeser.lambdatest.testng.RunnableWithException;
 
-public class FListTest {
+public class FListTest extends FreeSpec {
 
-	@DataProvider
-	public Iterator<Object[]> dataFoldLeft() {
-		@SuppressWarnings("serial")
-		class Data extends LinkedList<Object[]> {
-			<T> void data(final T expected, final T left, final F2<T, T, T> function, final T... list) {
-				add(new Object[] { expected, left, function, list });
-			}
-		}
-		;
-
-		final Data data = new Data();
+	public FListTest() {
 
 		final F2<Integer, Integer, Integer> add = new F2<Integer, Integer, Integer>() {
 			public Integer apply(final Integer p1, final Integer p2) {
 				return p1 + p2;
 			}
 		};
+
+		testFoldLeft(0, 0, add);
+		testFoldLeft(1, 0, add, 1);
+		testFoldLeft(3, 0, add, 1, 2);
+		testFoldLeft(6, 0, add, 1, 2, 3);
+
 		final F2<Integer, Integer, Integer> multiply = new F2<Integer, Integer, Integer>() {
 			public Integer apply(final Integer p1, final Integer p2) {
 				return p1 * p2;
 			}
 		};
 
-		data.data(6, 0, add, 1, 2, 3);
-		data.data(6, 1, multiply, 1, 2, 3);
+		testFoldLeft(1, 1, multiply);
+		testFoldLeft(1, 1, multiply, 1);
+		testFoldLeft(2, 1, multiply, 1, 2);
+		testFoldLeft(6, 1, multiply, 1, 2, 3);
+		testFoldLeft(24, 1, multiply, 1, 2, 3, 4);
 
-		return data.iterator();
-	}
+		final F1<String, String> noConvert = null;
 
-	@Test(dataProvider = "dataFoldLeft")
-	public <T> void testFoldLeft(final T expected, final T left, final F2<T, T, T> function, final T[] list) {
-		assertEquals(foldLeft(list, left, function), expected);
-		assertEquals(foldLeft(Arrays.asList(list), left, function), expected);
-	}
+		testMkString("ABC", noConvert, null, null, null, "A", "B", "C");
+		testMkString("ABC", noConvert, null, "", null, "A", "B", "C");
+		testMkString("ABC", noConvert, "", null, null, "A", "B", "C");
+		testMkString("ABC", noConvert, null, null, "", "A", "B", "C");
+		testMkString("ABC", noConvert, "", "", "", "A", "B", "C");
+		testMkString("A,B,C", noConvert, null, ",", "", "A", "B", "C");
+		testMkString("[A,B,C]", noConvert, "[", ",", "]", "A", "B", "C");
+		testMkString("[A,B,null]", noConvert, "[", ",", "]", "A", "B", null);
 
-	@DataProvider
-	public Iterator<Object[]> dataMkString() {
-		@SuppressWarnings("serial")
-		class Data extends LinkedList<Object[]> {
-			void data(final String expected, final String prefix, final String sep, final String suffix,
-					final Object... elements) {
-				add(new Object[] { expected, null, prefix, sep, suffix, elements });
-			}
-
-			void dataConvert(final String expected, final F1<?, String> convert, final String prefix, final String sep,
-					final String suffix,
-					final Object... elements) {
-				add(new Object[] { expected, convert, prefix, sep, suffix, elements });
-			}
-		}
-		;
-
-		final Data data = new Data();
-
-		data.data("ABC", null, null, null, "A", "B", "C");
-		data.data("ABC", null, "", null, "A", "B", "C");
-		data.data("ABC", "", null, null, "A", "B", "C");
-		data.data("ABC", null, null, "", "A", "B", "C");
-		data.data("ABC", "", "", "", "A", "B", "C");
-		data.data("A,B,C", null, ",", "", "A", "B", "C");
-		data.data("[A,B,C]", "[", ",", "]", "A", "B", "C");
-		data.data("[A,B,null]", "[", ",", "]", "A", "B", null);
-
-		final F1<String, String> convert = new F1<String, String>() {
+		final F1<String, String> nullSafeConvert = new F1<String, String>() {
 			public String apply(final String param) {
 				return param == null ? "" : param;
 			}
 		};
 
-		data.dataConvert("[A,B,]", convert, "[", ",", "]", "A", "B", null);
+		testMkString("[A,B,]", nullSafeConvert, "[", ",", "]", "A", "B", null);
 
-		return data.iterator();
 	}
 
-	@Test(dataProvider = "dataMkString")
+	public <T> void testFoldLeft(final T expected, final T left, final F2<T, T, T> function, final T... elements) {
+		test("FList.foldLeft: " + Arrays.deepToString(elements), new RunnableWithException() {
+			public void run() throws Exception {
+				assertEquals(foldLeft(elements, left, function), expected);
+				assertEquals(foldLeft(Arrays.asList(elements), left, function), expected);
+			}
+		});
+	}
+
 	public <T> void testMkString(final String expected, final F1<T, String> convert, final String prefix,
-			final String sep, final String suffix,
-			final T[] elements) {
-		assertEquals(mkString(Arrays.asList(elements), prefix, sep, suffix, convert), expected);
-		assertEquals(mkString(elements, prefix, sep, suffix, convert), expected);
-		if (convert == null) {
-			assertEquals(mkString(Arrays.asList(elements), prefix, sep, suffix), expected);
-			assertEquals(mkString(elements, prefix, sep, suffix), expected);
-		}
+			final String sep, final String suffix, final T... elements) {
+		test("FList.mkString should result in " + expected, new RunnableWithException() {
+			public void run() throws Exception {
+				assertEquals(mkString(Arrays.asList(elements), prefix, sep, suffix, convert), expected);
+				assertEquals(mkString(elements, prefix, sep, suffix, convert), expected);
+				if (convert == null) {
+					assertEquals(mkString(Arrays.asList(elements), prefix, sep, suffix), expected);
+					assertEquals(mkString(elements, prefix, sep, suffix), expected);
+				}
+			}
+		});
 	}
+
 }
