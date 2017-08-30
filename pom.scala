5290891dc3f2b1d93161089661c0f4b930ff064c
Tobias Roeser <le.petit.fou@web.de>
2017-08-30 08:44:31 +0200

    Formatted pom.scala

--- pom.scala
+++ pom.scala
@@ -19,24 +19,31 @@ Model(
   developers = Seq(
     Developer(
       email = "le.petit.fou@web.de",
-      name = "Tobias Roeser")),
+      name = "Tobias Roeser"
+    )
+  ),
   licenses = Seq(
     License(
       name = "Apache License, Version 2.0",
       url = "http://www.apache.org/licenses/LICENSE-2.0.txt",
-      distribution = "repo")),
+      distribution = "repo"
+    )
+  ),
   scm = Scm(
-    url = "https://github.com/ToToTec/de.tototec.utils.functional"),
+    url = "https://github.com/ToToTec/de.tototec.utils.functional"
+  ),
   dependencies = Seq(
     "org.testng" % "testng" % "6.9.10" % "test",
-    "de.tototec" % "de.tobiasroeser.lambdatest" % "0.2.3" % "test"),
+    "de.tototec" % "de.tobiasroeser.lambdatest" % "0.2.3" % "test"
+  ),
   properties = Map(
     "bundle.namespace" -> projectName,
     "project.build.sourceEncoding" -> "UTF-8",
     "maven.compiler.source" -> "1.5",
     "maven.compiler.target" -> "1.5",
     "maven.compiler.testTarget" -> "1.8",
-    "maven.compiler.testSource" -> "1.8"),
+    "maven.compiler.testSource" -> "1.8"
+  ),
   build = Build(
     plugins = Seq(
       // Check that no Java6+ API is used
@@ -46,12 +53,17 @@ Model(
           Execution(
             id = "signature-check",
             phase = "test",
-            goals = Seq("check"))),
+            goals = Seq("check")
+          )
+        ),
         configuration = Config(
           signature = Config(
             groupId = "org.codehaus.mojo.signature",
             artifactId = "java15",
-            version = "1.0"))),
+            version = "1.0"
+          )
+        )
+      ),
       // Package OSGi-compatible JAR and check version against previous release version
       Plugin(
         "org.apache.felix" % "maven-bundle-plugin" % "3.2.0",
@@ -61,13 +73,20 @@ Model(
             phase = "verify",
             goals = Seq("baseline"),
             configuration = Config(
-              failOnError = "false"))),
+              failOnError = "false"
+            )
+          )
+        ),
         dependencies = Seq(
-          Deps.bndLib),
+          Deps.bndLib
+        ),
         configuration = Config(
           unpackBundle = "true",
           instructions = Config(
-            _include = "osgi.bnd"))),
+            _include = "osgi.bnd"
+          )
+        )
+      ),
       // Use Asciidoclet processor instead of standard Javadoc
       Plugin(
         "org.apache.maven.plugins" % "maven-javadoc-plugin" % "2.10.3",
@@ -77,11 +96,17 @@ Model(
           docletArtifact = Config(
             groupId = Deps.asciiDoclet.groupId.get,
             artifactId = Deps.asciiDoclet.artifactId,
-            version = Deps.asciiDoclet.version.get),
+            version = Deps.asciiDoclet.version.get
+          ),
           overview = "README.adoc",
           additionalparam = s"""--base-dir "$${project.basedir}"
             | --attribute "name=${projectName}"
 						| --attribute "version=${projectVersion}"
 						| --attribute "functionalversion=${projectVersion}"
 						| --attribute "title-link=https://github.com/ToToTec/de.tototec.utils.functional[${projectName} ${projectVersion}]"
-						| --attribute "env-asciidoclet=true"""".stripMargin)))))
+						| --attribute "env-asciidoclet=true"""".stripMargin
+        )
+      )
+    )
+  )
+)
