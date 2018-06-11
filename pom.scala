16c6d4be298bd472f7dd212de09862f576e86a47
Tobias Roeser <le.petit.fou@web.de>
2018-06-11 15:27:24 +0200

    Version bump lambdatest 0.5.0

--- pom.scala
+++ pom.scala
@@ -8,7 +8,7 @@ object Deps {
   val asciiDoclet = "org.asciidoctor" % "asciidoclet" % "1.5.4"
   val antContrib = "ant-contrib" % "ant-contrib" % "1.0b3"
   val bndLib = "biz.aQute.bnd" % "biz.aQute.bndlib" % "3.2.0"
-  val lambdatest = "de.tototec" % "de.tobiasroeser.lambdatest" % "0.4.0"
+  val lambdatest = "de.tototec" % "de.tobiasroeser.lambdatest" % "0.5.0"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.25"
   val testng = "org.testng" % "testng" % "6.14.3"
